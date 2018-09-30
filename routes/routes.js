31b368ad64f556b1176f347979eb0946216fef62
bobomurod <bobomurod.boboev@yandex.ru>
2018-09-30 21:53:38 +0500

    prepare to post request

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 var hello = require('../models/hello.js');
 var minting = require('../models/minting.js');
 var sending = require('../models/sending.js');
+var balanceOf = require('../models/balanceOf.js')
 
 var appRouter = function(app) {
     app.get('/hello', function (req, res, next) {
@@ -21,7 +22,11 @@ var appRouter = function(app) {
         console.log("Done.");
     })
 
-
+    app.get('/balanceOf', function (req, res, next) {
+        console.log("Responding...");
+        res.status(200).json(balanceOf());
+        console.log("Done.");
+    })
 }
 
 module.exports = appRouter

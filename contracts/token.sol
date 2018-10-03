8ce122536d196f520cd206d2e9543e51b28b02c0
bobomurod <bobomurod.boboev@yandex.ru>
2018-10-04 00:50:20 +0500

    token.sol

---
+++ contracts/token.sol
@@ -0,0 +1,162 @@
+// делаем токен ёпта 
+pragma solidity ^0.4.24;
+
+/**
+ * @title SafeMath - библиотечка 
+ * @dev Безопасные математические операции (анти-переполнение буфера)
+ */
+
+ // Уножение
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+
+// Деление
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+// Минус
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+
+// Плюс
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @title ERC20Basic
+ * @dev Самая простая версия ERC20 интерфейса
+ * @dev подробно смотрите на https://github.com/ethereum/EIPs/issues/179
+ */
+
+contract ERC20Basic {
+    uint256 public totalSupply;  // totalSupply - общая эмиссия
+    function balanceOf(address who) public constant returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+/**
+* @title ERC20 интерфэйс
+* @dev смотрите в гите 
+ */
+
+contract ERC20 is ERC20Basic {
+    function allowance(address owner, address spender) public constant returns (uint256);
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+    function approve(address spender, uint256 value) public returns (bool);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract BasicToken is ERC20Basic {
+    using SafeMath for uint256;
+    mapping (address=>uint256) balances;
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[msg.sender]);
+
+        // SafeMath.sub не позволит отправить токенов если нет нужного кол-во на балансе
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;        
+    }
+
+    function balanceOf(address _owner) public constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+}
+
+contract StandardToken is ERC20, BasicToken {
+    mapping (address =>mapping (address=>uint256)) internal allowed;
+    
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+
+    function increaseApproval(address _spender, uint _addedValue) public returns (bool success) {
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+
+    function decreaseApproval (address _spender, uint _subtractedValue) public returns (bool success) {
+    uint oldValue = allowed[msg.sender][_spender];
+    if (_subtractedValue > oldValue) {
+      allowed[msg.sender][_spender] = 0;
+    } else {
+      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+    }
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+}
+
+// contract BurnableToken is StandardToken {
+
+//     event Burn(address indexed burner, uint256 value);
+
+//     /**
+//      * @dev Burns a specific amount of tokens.
+//      * @param _value The amount of token to be burned.
+//      */
+//     function burn(uint256 _value) public {
+//         require(_value*10**18 > 0);
+//         require(_value*10**18 <= balances[msg.sender]);
+//         // no need to require value <= totalSupply, since that would imply the
+//         // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+
+//         address burner = msg.sender;
+//         balances[burner] = balances[burner].sub(_value*10**18);
+//         totalSupply = totalSupply.sub(_value*10**18);
+//         Burn(burner, _value*10**18);
+//     }
+// }
+
+contract Rahmat is StandardToken {
+    event RahmatHasDeployed(uint time);
+    string public constant name = "rahmat";
+    string public constant sympol = "rahmat";
+    uint8 public constant decimals = 0;
+    uint256 public constant INITIAL_SUPPLY = 1000000 * (10 ** uint256(decimals));
+
+    function rahmat() {
+        totalSupply = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+        RahmatHasDeployed(now);
+    }
+}

5cb246f0a775e904ea23b68ef18d0d27b278763d
bobomurod <bobomurod.boboev@yandex.ru>
2018-12-15 01:35:43 +0500

    commit

--- controllers/minting.controller.js
+++ controllers/minting.controller.js
@@ -1,29 +1,49 @@
-const Holder = require('../models/holder.model');
-
+const Holder = require("../models/holder.model");
 
 exports.minting_test = function(req, res) {
-    res.send("Minting test");
-}
+  res.send("Minting test");
+};
 
 exports.minting_mint = function(req, res) {
-    
-    // Holder.findOneAndUpdate({wallet: req.body.wallet}, {$set: {$balance: balance + req.body.value}}, 
-    //     function(err, holder){
-    //         if(err) {
-    //             res.send(err)
-    //         }
-    //     })
-    // res.send("true")
+  // Holder.findOneAndUpdate({wallet: req.body.wallet}, {$set: {$balance: balance + req.body.value}},
+  //     function(err, holder){
+  //         if(err) {
+  //             res.send(err)
+  //         }
+  //     })
+  // res.send("true")
 
-    Holder.findOne({wallet: req.body.wallet}, function(err, holder){
+  Holder.findOne({ wallet: req.body.wallet }, function(err, holder) {
+    if (err) {
+      res.send(err);
+    }
+    Holder.findOneAndUpdate(
+      { wallet: req.body.wallet },
+      { $set: { balance: holder.balance + req.body.value } },
+      function(err, result) {
         if (err) {
-            res.send(err)
-            }
-        Holder.findOneAndUpdate({wallet: req.body.wallet}, {$set: { balance : holder.balance + req.body.value}}, function(err, result){
-            if (err) {
-                res.send(err)
-            }
-            res.send(result);
-        })
-    })
-}
+          res.send(err);
+        }
+        res.send(result);
+      }
+    );
+  });
+};
+
+exports.minting_easy = function(req, res) {
+  Holder.findOne({ wallet: req.body.wallet }, function(err, holder) {
+    if (err) {
+      res.send(err);
+    }
+    Holder.findOneAndUpdate(
+      { wallet: req.body.wallet },
+      { $set: { balance: holder.balance + req.body.value } },
+      function(err, result) {
+        if (err) {
+          res.send(err);
+        }
+        res.send(result);
+      }
+    );
+  });
+};
--- routes/minting.route.js
+++ routes/minting.route.js
@@ -1,14 +1,14 @@
 // Здесь все роутинги касательно выпуска валюты
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const minting_controller = require('../controllers/minting.controller');
+const minting_controller = require("../controllers/minting.controller");
 
 // тестовый роутинг
 
-router.get('/test', minting_controller.minting_test);
-router.post('/mint', minting_controller.minting_mint);
-
+router.get("/test", minting_controller.minting_test);
+router.post("/mint", minting_controller.minting_mint);
+router.post("/easy", minting_controller.minting_easy);
 
 module.exports = router;

45c8ffeaf9da5f78a2a5e48d748d8a57fb0f392c
bobomurod <bobomurod.boboev@yandex.ru>
2019-01-14 01:38:47 +0500

    optimized creating user at first transaction

--- controllers/minting.controller.js
+++ controllers/minting.controller.js
@@ -34,25 +34,35 @@ exports.minting_mint = function(req, res) {
 exports.minting_easy = function(req, res) {
   Holder.findOne({ wallet: req.body.wallet }, function(err, holder) {
     if (err) {
-      let holder = new Holder(
-        {
-            wallet: req.body.wallet,
-            balance: req.body.balance,
-            reg_date: new Date(),
-            reg_timestamp: new Timestamp(),
-            used: 0,
-            level: 0
-            
-        }
-      )
+      
       res.send(err);
     }
     Holder.findOneAndUpdate(
       { wallet: req.body.wallet },
-      { $set: { balance: holder.balance + (((req.body.value)/100)*1) } },
+      { $set: { balance: Holder.balance + (((req.body.value)/100)*1) } },
       function(err, result) {
         if (err) {
-          res.send(err);
+          let holder = new Holder(
+            {
+                wallet: req.body.wallet,
+                balance: (((req.body.value)/100)*1),
+                reg_date: new Date(),
+                reg_timestamp: Date.now(),
+                used: 0,
+                level: 0
+                
+            }
+          );
+          holder.save(function (err) {
+
+            if (err) {
+                res.send("wallet creating error, maybe wallet exists.")
+                return next(err);
+            }
+            console.log('Holder Created successfully')
+         
+        });
+          res.send("created");
         }
         res.send(result);
       }

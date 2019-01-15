f4ca24c7fac01c962468a6763611eafa9f2f308f
bobomurod <bobomurod.boboev@yandex.ru>
2019-01-15 21:57:53 +0500

    minting information can be stored in mongoDb

--- models/minting.model.js
+++ models/minting.model.js
@@ -1,7 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-var appMinting = function(address, quantity) {
-  return true;
-};
-module.exports = appMinting;
+// var appMinting = function(address, quantity) {
+//   return true;
+// };
+// module.exports = appMinting;
+
+
+let MintingSchema = new Schema({
+  initator_id: {type: String, required: false},
+  minting_id: {type: Number, required: true},
+  wallet: {type: Number, required: true},
+  value: {type: Number, required: true},
+  data: {type: DataCue, required: true},
+  timestamp: {type: Number, required: true}
+})
+
+module.exports = mongoose.model('Mint', MintingSchema)

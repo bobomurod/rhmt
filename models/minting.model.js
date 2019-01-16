b182a8d14c8188ae24fa519efbc4c02b2588071f
bobomurod <bobomurod.boboev@yandex.ru>
2019-01-16 07:21:15 +0500

    minting model undo

--- models/minting.model.js
+++ models/minting.model.js
@@ -7,13 +7,13 @@ const Schema = mongoose.Schema;
 // module.exports = appMinting;
 
 
-let MintingSchema = new Schema({
-  initator_id: {type: String, required: false},
-  minting_id: {type: Number, required: true},
-  wallet: {type: Number, required: true},
-  value: {type: Number, required: true},
-  data: {type: DataCue, required: true},
-  timestamp: {type: Number, required: true}
-})
+// let MintingSchema = new Schema({
+//   initator_id: {type: String, required: false},
+//   minting_id: {type: Number, required: true},
+//   wallet: {type: Number, required: true},
+//   value: {type: Number, required: true},
+//   data: {type: DataCue, required: true},
+//   timestamp: {type: Number, required: true}
+// })
 
-module.exports = mongoose.model('Mint', MintingSchema)
+// module.exports = mongoose.model('Mint', MintingSchema)

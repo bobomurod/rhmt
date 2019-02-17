28c8a679a4f709c6611df5932efb0b155da99df8
bobomurod <bobomurod.boboev@yandex.ru>
2019-02-18 02:16:54 +0500

    construction of testing scripts

--- test.js
+++ test.js
@@ -1,9 +1,48 @@
-var request = require('request');
-async function foo(){
-    let promise = new Promise((resolve, reject)=>{request.get('http://go.btc.uz:9994/holders', function(error, response, body){
-        console.log(response.body)
-    })})
+const axios = require("axios");
+const url = "http://go.btc.uz:9994/holders";
+/////////////////////////   GET HOLDERS     ///////////////////////////////////
+const getHolders = async url => {
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+var g = 1;
+for(i=0; i != 1; i++){
+getHolders(url).then(()=>{console.log(g++)});
 }
-for (i=0; i!= 1000; i++){
-foo().then(()=>{console.log(i)})
-}
+
+/////////////////////////   GET LEVEL BY MBSID    ///////////////////////////////////
+
+const getLevelByMbsid = async url => {
+    try {
+        const response = await axios.post(url, {
+            mbsid: '777'
+        });
+        const data = response.data;
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+getLevelByMbsid('http://go.btc.uz:9994/levels/getLevelByMBSID')
+
+/////////////////////////   GET LEVEL BY MSISDN    ///////////////////////////////////
+
+const getLevelByMsisdn = async url => {
+    try {
+        const response = await axios.post(url, {
+            wallet: '998994955409'
+        });
+        const data = response.data;
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+getLevelByMsisdn('http://go.btc.uz:9994/levels/getLevelByMSISDN')

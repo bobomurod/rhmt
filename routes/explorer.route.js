fa79da5c5c6aa93e7c1049e0409aff3e81dfdfe9
bobomurod <bobomurod.boboev@yandex.ru>
2019-03-23 00:12:11 +0500

    proper test

--- routes/explorer.route.js
+++ routes/explorer.route.js
@@ -1,1 +1,12 @@
-const explorer_controller = require('')
+//Здесь все роутинги для вывода истории по разным операциям 
+const explorer = require('express');
+const router = express.Router();
+
+const explorer_controller = require('../controllers/explorer.controller');
+
+//testng route
+
+
+router.get('/test', explorer_controller.test)
+
+module.exports = router;

3b06948319e53ebcf4f4e0687decd309d7db4747
bobomurod <bobomurod.boboev@yandex.ru>
2019-03-23 14:16:51 +0500

    controller for auth

---
+++ controllers/auth.controller.js
@@ -0,0 +1,56 @@
+const JWT = require('jsonwebtoken');
+const Users = require('../models/auth.model')
+
+exports.test = (req, res, next) => {
+    res.json({
+        message: "test from auth route "
+    })
+}
+
+exports.signup = (req, res, next) => {
+    Users.findOne({ username: req.body.username }, (err, result) => {
+        if (err) {
+            res.status(400);
+            next(err);
+        } else if (result) {
+            console.log("User exists");
+            res.json({
+                message: "User exists"
+            })
+        } else {
+            let user = new Users({
+                username: req.body.username,
+                password: req.body.password
+            })
+            user.save( (err, doc) => {
+                if (err) {
+                    res.status(400);
+                    next(err);
+                } else {
+                    res.json({
+                        message: "User reged "
+                    })
+                }
+            } )
+        }
+    })
+}
+
+exports.login = (req, res, next) => {
+    Users.findOne({ username: req.body.username }, (err, result) => {
+        if (err) {
+            res.status(400);
+            next(err);
+        } else {
+            if (result.username == req.body.username && result.password == req.body.password) {
+                res.json({
+                    message: "logged in"
+                })
+            } else {
+                res.json({
+                    message: "cannot login"
+                })
+            }
+        }
+    })
+}
